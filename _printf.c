48b6c39d57367e09a05b0183339da14b4a154de6
Afokenelly <112502259+Afokenelly@users.noreply.github.com>
2023-10-17 07:35:57 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,12 +3,12 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Entry point
+ * ur_printf - Entry point
  * @format: format.
  * Return: Printed chars.
  */
 
-int _printf(const char *format, ...)
+int ur_printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
